4d78b37f2d5bc2b21c8e3bc3daa868fafb67936e
brooktewabe <brooktewabe00@gmail.com>
2024-08-10 10:07:15 +0300

    'books-ui'

--- src/Pages/Books.jsx
+++ src/Pages/Books.jsx
@@ -53,7 +53,7 @@ const Books = () => {
 
   return (
     <>
-      <section className="bg-indigo-50">
+      <section className="bg-indigo-50 h-screen">
         <div className="container m-auto py-10 px-6">
           <div>
             <br />
